b42368e3448d6c77e6ffd3794af75aab1782e905
Jesús Perales <22j.perales@gmail.com>
2025-03-05 09:50:08 -0400

    feat(apps): add users and media_files models
    - Added MediaFile model in media.
    - Added custom User model with admin and author roles.
    - Added managers and proxies models for each role.
    - Added AuthorProfile model to save authors data.
    - Added SocialAccount model for social networks.

--- apps/media_files/models.py
+++ apps/media_files/models.py
@@ -1,3 +1,67 @@
-from django.db import models
+import os
 
-# Create your models here.
+from django.core.exceptions import ValidationError
+from django.db import models
+from PIL import Image
+
+from apps.utils.base_model import BaseModel
+
+
+class MediaFile(BaseModel):
+    class Type(models.TextChoices):
+        IMAGE = 'image'
+        VIDEO = 'video'
+        AUDIO = 'audio'
+
+    valid_extensions = {
+        Type.IMAGE: ['jpg', 'jpeg', 'png', 'gif', 'webp', 'svg'],
+        Type.VIDEO: ['mp4', 'webm'],
+        Type.AUDIO: ['mp3', 'aac', 'wav', 'ogg'],
+    }
+
+    @staticmethod
+    def _get_upload_path(instance, filename):
+        ext = filename.split('.')[-1].lstrip('.').lower()
+        file_type = instance._get_file_type(ext)
+        return os.path.join(str(instance.post.author.id), file_type, filename)
+
+    file = models.FileField(upload_to=_get_upload_path)
+    post = models.ForeignKey(
+        'content.Post', on_delete=models.CASCADE, related_name='media_files'
+    )
+    name = models.CharField(max_length=255)
+    type = models.CharField(choices=Type.choices, max_length=10)
+    size = models.PositiveIntegerField()
+    width = models.PositiveIntegerField(null=True)
+    height = models.PositiveIntegerField(null=True)
+
+    def __str__(self):
+        return self.name
+
+    def _get_file_type(self, ext):
+        for file_type, extensions in self.valid_extensions.items():
+            if ext in extensions:
+                return file_type
+        raise ValidationError(f'Invalid file type: .{ext} is not allowed.')
+
+    def _extract_image_metadata(self):
+        try:
+            with Image.open(self.file) as img:
+                self.width, self.height = img.size
+        except Exception as e:
+            raise ValidationError(f'Error extracting image metadata: {e}')
+
+    def save(self, *args, **kwargs):
+        if self.file:
+            self.name = os.path.basename(self.file.name)
+            ext = self.name.split('.')[-1].lstrip('.').lower()
+            self.type = self._get_file_type(ext)
+            self.size = self.file.size
+
+            if self.type == self.Type.IMAGE:
+                self._extract_image_metadata()
+            super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'Media File'
+        verbose_name_plural = 'Media Files'
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,76 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.core.signals import post_save
 from django.db import models
+from django.dispatch import receiver
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN = 'admin'
+        AUTHOR = 'author'
+
+    base_role = Role.ADMIN
+    role = models.CharField(
+        choices=Role.choices,
+        max_length=50,
+        default=base_role,
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.role = self.Role.ADMIN
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.username} ({self.role})'
+
+
+class AdminManager(BaseUserManager):
+    def get_queryset(self, *args, **kwargs):
+        return super().get_queryset(*args, **kwargs).filter(role=User.Role.ADMIN)
+
+
+class Admin(User):
+    objects = AdminManager()
+
+    class Meta:
+        proxy = True
+
+
+class AuthorManager(BaseUserManager):
+    def get_queryset(self, *args, **kwargs):
+        return super().get_queryset(*args, **kwargs).filter(role=User.Role.AUTHOR)
+
+
+class Author(User):
+    base_role = User.Role.AUTHOR
+    objects = AuthorManager()
+
+    class Meta:
+        proxy = True
+
+
+class AuthorProfile(models.Model):
+    user = models.OneToOneField(Author, on_delete=models.CASCADE, primary_key=True)
+    profile_picture = models.ImageField(upload_to=f'{user.id}/image/profile', null=True)
+    bio = models.TextField(null=True)
+
+    def __str__(self):
+        return f'{self.user.username} ({self.user.role}) profile'
+
+
+@receiver(post_save, sender=Author)
+def create_author_profile(sender, instance, created, **kwargs):
+    if created and instance.role == User.Role.AUTHOR:
+        AuthorProfile.objects.create(user=instance)
+
+
+class SocialAccount(models.Model):
+    name = models.CharField(max_length=255)
+    url = models.URLField(unique=True)
+    profile = models.ForeignKey(
+        AuthorProfile, on_delete=models.CASCADE, related_name='social_accounts'
+    )
+
+    def __str__(self):
+        return self.name
