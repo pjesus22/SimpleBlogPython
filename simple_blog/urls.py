c8677b4f0d56016d20db355b9abb35e386e8d1a5
Jesús Perales <22j.perales@gmail.com>
2025-03-10 19:13:58 -0400

    feat: add API routes for content and users apps
    - Included content and users app URLs under api/v1/ to organize API
    endpoints globally.

--- simple_blog/urls.py
+++ simple_blog/urls.py
@@ -14,9 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(
+        'api/v1/',
+        include(
+            [
+                path('', include('apps.content.urls')),
+                path('', include('apps.users.urls')),
+            ]
+        ),
+    ),
 ]
