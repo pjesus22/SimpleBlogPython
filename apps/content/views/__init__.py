08837e293c419b9a4d96f341342ec6c9656649c8
Jesús Perales <22j.perales@gmail.com>
2025-03-13 14:19:47 -0400

    refactor(content): add post and tag views to __init__.py
    - Added PostDetailView, PostListView, TagDetailView, and TagListView
    to __all__ for easier imports.

--- apps/content/views/__init__.py
+++ apps/content/views/__init__.py
@@ -1,6 +1,12 @@
 from .categories import CategoryDetailView, CategoryListView
+from .posts import PostDetailView, PostListView
+from .tags import TagDetailView, TagListView
 
 __all__ = [
     'CategoryDetailView',
     'CategoryListView',
+    'PostDetailView',
+    'PostListView',
+    'TagDetailView',
+    'TagListView',
 ]
