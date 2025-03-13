b3ba6a129c5f40b6880900cf084b213e0aedfc57
Jesús Perales <22j.perales@gmail.com>
2025-03-13 14:31:46 -0400

    refactor(users): improve list and detail views
    - Refactored views to handle requests and responses more efficiently,
    and added error handling for detail views.

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,4 +1,4 @@
-from django.http import JsonResponse
+from django.http import Http404, JsonResponse
 from django.views.generic import DetailView, ListView
 
 from .models import User
@@ -9,25 +9,32 @@ class UserListView(ListView):
     model = User
     context_object_name = 'users'
 
-    def render_to_response(self, context, **response_kwargs):
-        return JsonResponse(
-            {
-                'data': [
-                    UserSerializer.serialize_user(user) for user in context['users']
-                ]
-            },
-            safe=False,
-        )
+    def get(self, request, *args, **kwargs):
+        self.object_list = self.get_queryset()
+        context = self.get_context_data()
+        users = context['users']
+        response_data = {
+            'data': [UserSerializer.serialize_user(user) for user in users]
+        }
+        return JsonResponse(response_data, safe=False)
 
 
 class UserDetailView(DetailView):
     model = User
     context_object_name = 'user'
 
-    def render_to_response(self, context, **response_kwargs):
+    def get(self, request, *args, **kwargs):
+        try:
+            self.object = self.get_object()
+        except User.DoesNotExist:
+            raise Http404('User not found')
+
+        context = self.get_context_data()
         user = context['user']
-        response_data = {
-            'data': UserSerializer.serialize_user(user),
-            'included': UserSerializer.build_included_data(user),
-        }
-        return JsonResponse(response_data, safe=False)
+        response_data = {'data': UserSerializer.serialize_user(user)}
+        included = UserSerializer.build_included_data(user)
+
+        if included:
+            response_data['included'] = included
+
+        return JsonResponse(response_data)
