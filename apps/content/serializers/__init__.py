e341afb1ebed31f686fcaf120dc6c13ec9ad8ed7
Jesús Perales <22j.perales@gmail.com>
2025-03-13 14:10:46 -0400

    refactor(content): add __init__.py to serializers
    - Added __init__.py to export CategorySerializer, PostSerializer, and
    TagSerializer for easier imports.

---
+++ apps/content/serializers/__init__.py
@@ -0,0 +1,9 @@
+from .categories import CategorySerializer
+from .posts import PostSerializer
+from .tags import TagSerializer
+
+__all__ = [
+    'CategorySerializer',
+    'PostSerializer',
+    'TagSerializer',
+]
