deba565f02909f233927fa2f062bb9831e8cdc7c
Jesús Perales <22j.perales@gmail.com>
2025-03-19 23:25:38 -0400

    refactor(views): update Category views
    - Replaced `ListView` and `DetailView` with Django's `View` class.
    - Added `post`, `patch`, and `delete` methods for CRUD operations.
    - Integrated permission decorators (`login_required`,
    `admin_required`).
    - Improved error handling and validation for requests.

--- apps/content/views/categories.py
+++ apps/content/views/categories.py
@@ -1,42 +1,71 @@
-from django.http import Http404, JsonResponse
-from django.views.generic import DetailView, ListView
+import json
+
+from django.core.exceptions import ValidationError
+from django.http import HttpResponse, JsonResponse
+from django.utils.decorators import method_decorator
+from django.views import View
+
+from apps.utils.permission_decorators import admin_required, login_required
 
 from ..models import Category
 from ..serializers import CategorySerializer
 
 
-class CategoryListView(ListView):
-    model = Category
-    context_object_name = 'categories'
+class CategoryListView(View):
+    http_method_names = ['get', 'post', 'head', 'options']
+
+    def get_queryset(self):
+        return Category.objects.all()
 
     def get(self, request, *args, **kwargs):
-        self.object_list = self.get_queryset()
-        context = self.get_context_data()
-        categories = context['categories']
         response_data = {
             'data': [
                 CategorySerializer.serialize_category(category)
-                for category in categories
+                for category in self.get_queryset()
             ]
         }
-        return JsonResponse(response_data, safe=False)
+        return JsonResponse(response_data)
 
-
-class CategoryDetailView(DetailView):
-    model = Category
-    slug_field = 'slug'
-    context_object_name = 'category'
-
-    def get(self, request, *args, **kwargs):
+    @method_decorator([login_required, admin_required])
+    def post(self, request, *args, **kwargs):
         try:
-            self.object = self.get_object()
-        except Http404:
-            return JsonResponse(
-                {'error': f'Category {kwargs["slug"]} not found'}, status=404
+            data = json.loads(request.body)
+            allowed_fields = {'name', 'description'}
+
+            if not set(data.keys()) <= allowed_fields:
+                return JsonResponse(
+                    {'error': f'Invalid fields: {", ".join(data.keys())}'},
+                    status=400,
+                )
+
+            category = Category(
+                name=data.get('name'), description=data.get('description')
             )
 
-        context = self.get_context_data()
-        category = context['category']
+            category.save()
+            return JsonResponse(
+                CategorySerializer.serialize_category(category), status=201
+            )
+        except json.JSONDecodeError as e:
+            return JsonResponse({'error': e.msg}, status=400)
+        except ValidationError as e:
+            return JsonResponse({'error': e.messages}, status=400)
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
+
+
+class CategoryDetailView(View):
+    http_method_names = ['get', 'patch', 'delete', 'head', 'options']
+
+    def get_object(self):
+        return Category.objects.filter(slug=self.kwargs.get('slug')).first()
+
+    def get(self, request, *args, **kwargs):
+        category = self.get_object()
+
+        if not category:
+            return JsonResponse({'error': 'Category not found'}, status=404)
+
         response_data = {'data': CategorySerializer.serialize_category(category)}
         included = CategorySerializer.build_included_data(category)
 
@@ -44,3 +73,48 @@ class CategoryDetailView(DetailView):
             response_data['included'] = included
 
         return JsonResponse(response_data)
+
+    @method_decorator([login_required, admin_required])
+    def patch(self, request, *args, **kwargs):
+        try:
+            category = self.get_object()
+
+            if not category:
+                return JsonResponse({'error': 'Category not found'}, status=404)
+
+            data = json.loads(request.body)
+            allowed_fields = {'description'}
+            invalid_fields = set(data) - allowed_fields
+
+            if invalid_fields:
+                return JsonResponse(
+                    {'error': f'Invalid fields: {", ".join(invalid_fields)}'},
+                    status=400,
+                )
+
+            for field in allowed_fields & data.keys():
+                setattr(category, field, data[field])
+
+            category.save()
+            return JsonResponse(
+                CategorySerializer.serialize_category(category), status=200
+            )
+        except json.JSONDecodeError as e:
+            return JsonResponse({'error': e.msg}, status=400)
+        except ValidationError as e:
+            return JsonResponse({'error': e.messages}, status=400)
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
+
+    @method_decorator([login_required, admin_required])
+    def delete(self, request, *args, **kwargs):
+        try:
+            category = self.get_queryset()
+
+            if not category:
+                return JsonResponse({'error': 'Category not found'}, status=404)
+
+            category.delete()
+            return HttpResponse(status=204)
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
