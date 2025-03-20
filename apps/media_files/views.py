ec05db1f0d8fb4a048161413085e55d54eb96b4c
Jesús Perales <22j.perales@gmail.com>
2025-03-19 23:58:34 -0400

    feat(apps/media_files): add MediaFileListView
    - Added `MediaFileListView` to handle listing of media files.
    - Integrated `login_required` and `admin_required` decorators for
    access control.
    - Used `MediaFileSerializer` to serialize media file data.

--- apps/media_files/views.py
+++ apps/media_files/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views import View
 
-# Create your views here.
+from ..utils.permission_decorators import admin_required, login_required
+from .models import MediaFile
+from .serializers import MediaFileSerializer
+
+
+class MediaFileListView(View):
+    http_method_names = ['get', 'head', 'options']
+
+    def get_queryset(self):
+        return MediaFile.objects.all()
+
+    @method_decorator([login_required, admin_required])
+    def get(self, request, *args, **kwargs):
+        media_files = self.get_queryset()
+        serialized_data = MediaFileSerializer.serialize_media_files(media_files)
+        return JsonResponse({'data': serialized_data})
