70437cd2efec9f5d09fc326155e9abf00443a5f7
Jesús Perales <22j.perales@gmail.com>
2025-04-18 19:01:36 -0400

    refactor(media_files): update view response structure and imports
    - Updated permission decorators import path to use new location
    - Improved response structure in MediaFileListView by creating explicit response_data object
    - Maintained same functionality while making code more consistent

--- apps/media_files/views.py
+++ apps/media_files/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 from django.utils.decorators import method_decorator
 from django.views import View
 
-from ..utils.permission_decorators import admin_required, login_required
+from ..utils.decorators import admin_required, login_required
 from .models import MediaFile
 from .serializers import MediaFileSerializer
 
@@ -15,6 +15,7 @@ class MediaFileListView(View):
 
     @method_decorator([login_required, admin_required])
     def get(self, request, *args, **kwargs):
-        media_files = self.get_queryset()
-        serialized_data = MediaFileSerializer.serialize_media_files(media_files)
-        return JsonResponse({'data': serialized_data})
+        response_data = {
+            'data': MediaFileSerializer.serialize_media_files(self.get_queryset())
+        }
+        return JsonResponse(response_data)
