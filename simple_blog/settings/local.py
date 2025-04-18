6a49caedbbcf25a32916f3fa48761beaee29e0a8
Jesús Perales <22j.perales@gmail.com>
2025-04-18 19:27:12 -0400

    chore(settings): configure local development environment
    - Added django_extensions to local INSTALLED_APPS
    - Configured SQLite database for local development
    - Set test database path to db_testing.sqlite3

--- simple_blog/settings/local.py
+++ simple_blog/settings/local.py
@@ -1,1 +1,12 @@
 from .base import *  # NOQA F403
+
+INSTALLED_APPS += [  # NOQA F405
+    'django_extensions',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db_testing.sqlite3',  # NOQA F405
+    }
+}
