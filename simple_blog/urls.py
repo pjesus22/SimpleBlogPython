7b5fae73e102a9fe98378a3509aa58f16871fb0c
Jesús Perales <22j.perales@gmail.com>
2025-04-22 19:37:46 -0400

    feat(urls): reorganize URL patterns and add health check
    - Move admin URL below API endpoints
    - Add new health check endpoint
    - Serve static/media files in DEBUG mode

--- simple_blog/urls.py
+++ simple_blog/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from simple_blog.health import health_check
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path(
         'api/v1/',
         include(
@@ -30,4 +33,10 @@ urlpatterns = [
             ]
         ),
     ),
+    path('admin/', admin.site.urls),
+    path('health/', health_check, name='health_check'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
