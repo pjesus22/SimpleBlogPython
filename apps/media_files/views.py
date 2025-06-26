a4ed2d01e9b6f018267bd2d5acd44731106cdf81
Jesús Perales <22j.perales@gmail.com>
2025-06-25 21:16:10 -0400

    feat(media): add detail view for media files
    - Introduce MediaFileDetailView to retrieve individual media files by
    ID, using JsonApiResponseBuilder for consistent JSON:API responses and
    improved error handling.

--- apps/media_files/views.py
+++ apps/media_files/views.py
@@ -1,6 +1,9 @@
-from django.http import JsonResponse
+from django.http import Http404, JsonResponse
+from django.shortcuts import get_object_or_404
 from django.utils.decorators import method_decorator
 from django.views import View
+
+from apps.utils.jsonapi_responses import JsonApiResponseBuilder as jarb
 
 from ..utils.decorators import admin_required, login_required
 from .models import MediaFile
@@ -19,3 +22,16 @@ class MediaFileListView(View):
             'data': MediaFileSerializer.serialize_media_files(self.get_queryset())
         }
         return JsonResponse(response_data)
+
+
+class MediaFileDetailView(View):
+    http_method_names = ['get', 'head', 'options']
+
+    @method_decorator([login_required, admin_required])
+    def get(self, request, *args, **kwargs):
+        try:
+            media_file = get_object_or_404(MediaFile, id=kwargs.get('id'))
+            response_data = MediaFileSerializer.serialize_media_file(media_file)
+            return jarb.ok(response_data)
+        except Http404:
+            return jarb.error(404, 'Not Found', 'Media file not found')
