9d8dfc91bde39c68d6f757962d275b493c502967
Jesús Perales <22j.perales@gmail.com>
2025-07-01 14:59:31 -0400

    test(serializers): refactor post serializer tests
    - Refactor post serializer tests for clarity and maintainability.
    - Add parametrized tests to cover combinations of tags and media files
    in relationships and included data.

--- tests/unit_tests/serializers/test_post_serializers.py
+++ tests/unit_tests/serializers/test_post_serializers.py
@@ -6,148 +6,156 @@ pytestmark = pytest.mark.django_db
 
 
 @pytest.fixture
-def post_inst(
-    author_factory,
-    post_factory,
-    category_factory,
-    tag_factory,
-    media_file_factory,
-    post_statistics_factory,
-    clean_media_dir,
-):
-    user = author_factory()
-    category = category_factory()
-    tags = tag_factory.create_batch(size=3)
-    media_files = media_file_factory.create_batch(size=2)
-    post = post_factory(
-        author=user,
-        category=category,
-        tags=tags,
-        media_files=media_files,
-    )
-    post_statistics = post_statistics_factory(post=post)  # NOQA: F841
-    return post
+def new_post(post_factory):
+    return post_factory()
 
 
-@pytest.fixture
-def post_inst_without_tags_or_media_files(
-    author_factory,
-    post_factory,
-    category_factory,
-    post_statistics_factory,
-    clean_media_dir,
-):
-    user = author_factory()
-    category = category_factory()
-    post = post_factory(
-        author=user,
-        category=category,
-    )
-    post_statistics = post_statistics_factory(post=post)  # NOQA: F841
-    return post
-
-
-def test_serialize_post_includes_relationships_when_requested(post_inst):
-    serialized_data = PostSerializer.serialize_post(
-        post_inst, include_relationships=True
-    )
-    expected_data = {
-        'author': {'data': {'type': 'users', 'id': str(post_inst.author.pk)}},
-        'category': {'data': {'type': 'categories', 'id': str(post_inst.category.pk)}},
-        'tags': {
-            'data': [
-                {'type': 'tags', 'id': str(tag.pk)} for tag in post_inst.tags.all()
-            ]
+def test_serialize_post_includes_relationships_when_requested(new_post):
+    data = PostSerializer.serialize_post(new_post, include_relationships=True)
+    expected = {
+        'author': {
+            'data': {
+                'type': 'users',
+                'id': str(new_post.author.pk),
+            }
         },
-        'media_files': {
-            'data': [
-                {'type': 'media_files', 'id': str(media_file.pk)}
-                for media_file in post_inst.media_files.all()
-            ]
+        'category': {
+            'data': {
+                'type': 'categories',
+                'id': str(new_post.category.pk),
+            }
+        },
+        'statistics': {
+            'data': {
+                'type': 'post_statistics',
+                'id': str(new_post.post_statistics.pk),
+            }
         },
     }
 
-    assert 'relationships' in serialized_data
-    assert serialized_data['relationships'] == expected_data
+    assert 'relationships' in data
+    assert all(key in data['relationships'] for key in expected.keys())
 
 
-def test_serialize_post_excludes_relationships_when_not_requested(post_inst):
-    serialized_data = PostSerializer.serialize_post(
-        post_inst, include_relationships=False
+def test_serialize_post_excludes_relationships_when_not_requested(new_post):
+    data = PostSerializer.serialize_post(new_post, include_relationships=False)
+
+    assert 'relationships' not in data
+
+
+@pytest.mark.parametrize(
+    'tag_count, media_file_count',
+    [
+        (2, 2),
+        (0, 0),
+        (0, 2),
+        (2, 0),
+    ],
+    ids=[
+        'tags_and_media_files',
+        'none',
+        'only_media_files',
+        'only_tags',
+    ],
+)
+def test_build_included_data_includes_correct_post_details(
+    post_factory, tag_factory, media_file_factory, tag_count, media_file_count
+):
+    tags = tag_factory.create_batch(size=tag_count) if tag_count > 0 else []
+    media_files = (
+        media_file_factory.create_batch(size=2) if media_file_count > 0 else []
     )
+    post = post_factory(tags=tags, media_files=media_files)
+    included = PostSerializer.build_included_data(post)
+    data = PostSerializer.serialize_post(post, include_relationships=True)
 
-    assert 'relationships' not in serialized_data
+    expected_items = []
 
-
-def test_build_included_data_includes_correct_post_details(post_inst):
-    included = PostSerializer.build_included_data(post_inst)
-    expected_tags = [
+    expected_items.append(
         {
-            'type': 'tags',
-            'id': str(tag.pk),
+            'type': 'users',
+            'id': str(post.author.pk),
             'attributes': {
-                'name': tag.name,
-                'slug': tag.slug,
-                'created_at': tag.created_at,
-                'updated_at': tag.updated_at,
+                'username': post.author.username,
+                'role': post.author.role,
             },
         }
-        for tag in post_inst.tags.all()
-    ]
-    expected_media_files = [
+    )
+
+    expected_items.append(
         {
-            'type': 'media_files',
-            'id': str(media_file.pk),
+            'type': 'categories',
+            'id': str(post.category.pk),
             'attributes': {
-                'file': media_file.file.name,
-                'name': media_file.name,
-                'type': media_file.type,
-                'size': media_file.size,
-                'width': media_file.width,
-                'height': media_file.height,
-                'created_at': media_file.created_at,
-                'updated_at': media_file.updated_at,
+                'name': post.category.name,
+                'description': post.category.description,
+                'slug': post.category.slug,
+                'created_at': post.category.created_at,
+                'updated_at': post.category.updated_at,
             },
         }
-        for media_file in post_inst.media_files.all()
-    ]
-    expected_author = {
-        'type': 'users',
-        'id': str(post_inst.author.pk),
-        'attributes': {
-            'username': post_inst.author.username,
-            'role': post_inst.author.role,
-        },
-    }
-    expected_category = {
-        'type': 'categories',
-        'id': str(post_inst.category.pk),
-        'attributes': {
-            'name': post_inst.category.name,
-            'description': post_inst.category.description,
-            'slug': post_inst.category.slug,
-            'created_at': post_inst.category.created_at,
-            'updated_at': post_inst.category.updated_at,
-        },
-    }
+    )
 
-    for expected_tag in expected_tags:
-        assert expected_tag in included
+    expected_items.append(
+        {
+            'type': 'post_statistics',
+            'id': str(post.post_statistics.pk),
+            'attributes': {
+                'share_count': post.post_statistics.share_count,
+                'like_count': post.post_statistics.like_count,
+                'comment_count': post.post_statistics.comment_count,
+                'created_at': post.post_statistics.created_at,
+                'updated_at': post.post_statistics.updated_at,
+            },
+        }
+    )
 
-    for expected_media_file in expected_media_files:
-        assert expected_media_file in included
+    if tag_count > 0 or media_file_count > 0:
+        expected_items.extend(
+            [
+                {
+                    'type': 'tags',
+                    'id': str(tag.pk),
+                    'attributes': {
+                        'name': tag.name,
+                        'slug': tag.slug,
+                        'created_at': tag.created_at,
+                        'updated_at': tag.updated_at,
+                    },
+                }
+                for tag in tags
+            ]
+        )
 
-    assert expected_author in included
+        expected_items.extend(
+            [
+                {
+                    'type': 'media_files',
+                    'id': str(media_file.pk),
+                    'attributes': {
+                        'file': str(media_file.file.url),
+                        'type': media_file.type.value,
+                        'created_at': media_file.created_at,
+                        'updated_at': media_file.updated_at,
+                    },
+                }
+                for media_file in media_files
+            ]
+        )
 
-    assert expected_category in included
+    included_set = {str(item) for item in included}
+    expected_set = {str(item) for item in expected_items}
 
-    expected_length = len(expected_tags) + len(expected_media_files) + 2
-    assert len(included) == expected_length
+    if tag_count:
+        tag_ids = {str(tag.id) for tag in tags}
+        rel_tag_ids = {item['id'] for item in data['relationships']['tags']['data']}
+        assert tag_ids == rel_tag_ids
+    if media_file_count:
+        mf_ids = {str(mf.id) for mf in media_files}
+        rel_mf_ids = {
+            item['id'] for item in data['relationships']['media_files']['data']
+        }
+        assert mf_ids == rel_mf_ids
 
-
-def test_build_included_data_excludes_tags_and_media_files_when_not_requested(
-    post_inst_without_tags_or_media_files,
-):
-    included = PostSerializer.build_included_data(post_inst_without_tags_or_media_files)
-    expected_length = 2
-    assert len(included) == expected_length
+    assert expected_set.issubset(included_set)
+    assert len(included) == len(expected_items)
