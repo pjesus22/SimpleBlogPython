d301b04af36a0454b9967c39725e51e7558fc8a4
Jesús Perales <22j.perales@gmail.com>
2025-07-09 16:57:26 -0400

    refactor(models): remove length limit from category description
    - The 255-character limit on category descriptions was unnecessarily
    restrictive.

--- apps/content/models/categories.py
+++ apps/content/models/categories.py
@@ -6,7 +6,7 @@ from apps.utils.base_model import BaseModel
 
 class Category(BaseModel):
     name = models.CharField(max_length=50, unique=True, null=False)
-    description = models.TextField(max_length=255, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
     slug = models.SlugField(max_length=50, unique=True, null=False)
 
     def __str__(self):
